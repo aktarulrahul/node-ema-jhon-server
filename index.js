243c55392241ca033cbf3ea2f9210786a710065a
aktarulrahul <contact.aktarul@gmail.com>
2021-10-25 20:49:28 +0600

    implement pagination

--- index.js
+++ index.js
@@ -37,8 +37,24 @@ async function mongodbCURD() {
     ------------------------------------- */
     app.get('/products', async (req, res) => {
       const cursor = productCollection.find({});
-      const products = await cursor.toArray();
-      res.send(products);
+      const count = await cursor.count();
+      console.log(req.query);
+      const page = req.query.page;
+      const size = parseInt(req.query.size);
+      let products;
+      if (page) {
+        products = await cursor
+          .skip(page * size)
+          .limit(size)
+          .toArray();
+      } else {
+        products = await cursor.toArray();
+      }
+
+      res.send({
+        count,
+        products,
+      });
     });
   } finally {
     // await client.close();
