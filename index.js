0b7e461364e1daecff54810e0f5c5b4bbb6339c4
aktarulrahul <contact.aktarul@gmail.com>
2021-10-26 12:56:28 +0600

    implement API for save order information in mongodb

--- index.js
+++ index.js
@@ -32,13 +32,14 @@ async function mongodbCURD() {
     ------------------------------------- */
     const database = client.db('ema_jhon');
     const productCollection = database.collection('products');
+    const orderCollection = database.collection('orders');
     /* ------------------------------------- 
     GET Products API
     ------------------------------------- */
     app.get('/products', async (req, res) => {
       const cursor = productCollection.find({});
       const count = await cursor.count();
-      console.log(req.query);
+      // console.log(req.query);
       const page = req.query.page;
       const size = parseInt(req.query.size);
       let products;
@@ -56,15 +57,36 @@ async function mongodbCURD() {
         products,
       });
     });
+    /* ------------------------------------- 
+    Use post to get data by keys
+    ------------------------------------- */
+    app.post('/products/bykeys', async (req, res) => {
+      const keys = req.body;
+      console.log(keys);
+      const query = { key: { $in: keys } };
+      const products = await productCollection.find(query).toArray();
+      res.json(products);
+    });
+    /* ------------------------------------- 
+    Add order API
+    ------------------------------------- */
+    app.post('/orders', async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.json(result);
+    });
+
+    /* ------------------------------------- 
+    Check API
+    ------------------------------------- */
+    app.get('/', (req, res) => {
+      res.send('Running Server');
+    });
   } finally {
     // await client.close();
   }
 }
 
 mongodbCURD().catch(console.dir);
-
-app.get('/', (req, res) => {
-  res.send('Running Server at', port);
-});
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
